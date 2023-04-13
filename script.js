31c8e4f921e300d79506fdaf29304d2c29272e41
MPyscho <123724212+MPyscho@users.noreply.github.com>
2023-04-13 10:53:43 +0200

    dos variable distintas

---
+++ script.js
@@ -0,0 +1,5 @@
+alert('Hola, a continuación se le pedirá un número para reaizar unas operaciones ');
+let nombreDeLaVariable = 'primerNumero';
+nombreDeLaVariable = prompt("introduzca un numero");
+let nombreDeLaVariable = 'segundoNumero';
+nombreDeLaVariable = prompt('Introduzca numero')

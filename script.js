e1eafbc3698a8b329139c4eb8a422e0dc745c72c
MPyscho <123724212+MPyscho@users.noreply.github.com>
2023-04-13 10:56:08 +0200

    script cambiado variable

--- script.js
+++ script.js
@@ -1,5 +1,3 @@
 alert('Hola, a continuación se le pedirá un número para reaizar unas operaciones ');
 let nombreDeLaVariable = 'primerNumero';
 nombreDeLaVariable = prompt("introduzca un numero");
-let nombreDeLaVariable = 'segundoNumero';
-nombreDeLaVariable = prompt('Introduzca numero')

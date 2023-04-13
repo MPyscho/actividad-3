6f1b7c814ec9d32f6a2e6938fdc72bca2289618f
MPyscho <123724212+MPyscho@users.noreply.github.com>
2023-04-13 11:08:56 +0200

    script

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
-let nombreDeLaVariable = 'primerNumero';
+let primerNumero;
 alert('Hola, a continuación se le pedirá un número para realizar unas operaciones ');
-nombreDeLaVariable = prompt("introduzca un numero");
+primerNumero = prompt("introduzca un numero");
+let segundoNumero;
+segundoNumero= prompt('introduzca un numero')

9539c9bf995550bfe108ef096a548e37bf8fe3b4
MPyscho <123724212+MPyscho@users.noreply.github.com>
2023-04-13 18:58:08 +0200

    para que se sumen como números y no caracteres

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
-let primerNumero;
+let primerNumero = Number;
 alert('Hola, a continuación se le pedirá un número para realizar unas operaciones ');
 primerNumero = prompt("introduzca un numero");
-let segundoNumero;
+let segundoNumero = Number;
 segundoNumero= prompt('introduzca un numero')
 vsuma = primerNumero+segundoNumero;
 alert('La suma es '+vsuma)
